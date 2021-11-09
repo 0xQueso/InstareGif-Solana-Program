0eb71987c6b55d435a002ad4e19a58dde2dce5a7
0xqueso <0xqueso@gmail.com>
2021-11-09 14:45:07 +0800

    initial commit

---
+++ tests/instagif.js
@@ -0,0 +1,61 @@
+const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
+
+// describe('instagif', () => {
+//
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.Provider.env());
+//
+//   it('Is initialized!', async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Instagif;
+//     const tx = await program.rpc.initialize();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+const main = async() => {
+  console.log("🚀 Starting test...")
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Instagif;
+
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+  console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('GIF COUNT', account.totalGifs.toString());
+
+  await program.rpc.addGif("test.gif",{
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    }
+  })
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('NEW GIF COUNT', account.totalGifs.toString());
+  console.log('GIF LIST', account.gifList);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
